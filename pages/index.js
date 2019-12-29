c281bf8bac95c220d0cfe8614afb75c779e91a16
Magnus Havgry <magnushavgry@gmail.com>
2019-12-29 19:16:22 +0100

    Set up Next

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>NextF1.com</title>
+    </Head>
+
+    Hi!
+  </div>
+)
+
+export default Home
