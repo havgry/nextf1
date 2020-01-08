d9b4ab6424496d86c7a1c1d84ad75d5fc9352c44
Magnus Havgry <magnushavgry@gmail.com>
2020-01-08 19:32:25 +0100

    Don't mount collapsed events to improve rendering speed

--- components/event.jsx
+++ components/event.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Collapse } from 'react-collapse'
+import { UnmountClosed as Collapse } from 'react-collapse'
 import Session from './session'
 import { getDate, getFullDate, groupByDayName, getStartEndDates } from '../utils'
 import { STATUS } from '../utils/enums'
@@ -47,9 +47,9 @@ const Event = ({
           </div>
         </a>
       </header>
-      <Collapse isOpened={isVisible}>
-        <main className={`main main--${isVisible ? 'visible' : 'hidden'}`}>
-          <ol>
+      <main className={`main main--${isVisible ? 'visible' : 'hidden'}`}>
+        <Collapse isOpened={isVisible}>
+          <ol className="sessions-list">
             {Object.keys(sessionsByDay).map((day) => {
               const dailySessions = sessionsByDay[day]
               return (
@@ -75,21 +75,20 @@ const Event = ({
               )}
             )}
           </ol>
-        </main>
-        {status === STATUS.UNCONFIRMED &&
-          <footer>
-            <div className="footer-container">  
-              <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="exclamation-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="icon">
-                <path fill="currentColor" d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
-              </svg>
-              <span title="Preliminary session times are based on 2019 schedule">
-                Session times yet to be confirmed for 2020
-              </span>
-            </div>
-          </footer>
-        }
-      </Collapse>
-
+          {status === STATUS.UNCONFIRMED &&
+            <footer>
+              <div className="footer-container">  
+                <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="exclamation-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="icon">
+                  <path fill="currentColor" d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
+                </svg>
+                <span title="Preliminary session times are based on 2019 schedule">
+                  Session times yet to be confirmed for 2020
+                </span>
+              </div>
+            </footer>
+          }
+        </Collapse>
+      </main>
       <style jsx>{`
         h1 {
           margin: 0;
@@ -119,9 +118,6 @@ const Event = ({
         }
 
         .main {
-          font-size: 0.8rem;
-          text-transform: uppercase;
-          padding: 0.5rem 0;
           transition: opacity .35s ease;
         }
 
@@ -145,6 +141,12 @@ const Event = ({
 
         .sessions {
           width: 60%;
+        }
+
+        .sessions-list {
+          font-size: 0.8rem;
+          text-transform: uppercase;
+          padding: 0.5rem 0;
         }
 
         section {
