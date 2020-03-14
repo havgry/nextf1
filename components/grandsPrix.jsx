8226b4ffa8471439b152ab6c5b0bb6566352a02f
Magnus Havgry <magnushavgry@gmail.com>
2020-03-14 14:27:51 +1300

    COVID-19 update 2

--- components/grandsPrix.jsx
+++ components/grandsPrix.jsx
@@ -7,11 +7,19 @@ const GrandsPrix = () => {
   return (
     <>
       <b>
-        Expect further cancellations the next coming months due to COVID-19 🤢
+        Corona update: Expect further cancellations and postponements the next coming months due to COVID-19 🤢
       </b>
+      <p>
+        "As a result, Formula 1 and the FIA expect to begin the Championship in Europe at the end of May but given the sharp increase in COVID-19 cases in Europe in recent days, this will be regularly reviewed."
+      </p>
       <List
         items={grandsPrixData.events}
       />
+      <style jsx>{`
+        p {
+          font-size: 0.8em;
+        }
+      `}</style>
     </>
   )
 }
